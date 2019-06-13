3c85510cf366a361b0748a53680a41b30f7a7d31
Kalin Chernev <kalin.chernev@gmail.com>
2019-06-13 09:22:29 +0300

    Refactoring

--- gatsby/packages/gatsby-source-drupal-jsonapi-eci/lib/addNodeTranslations.js
+++ gatsby/packages/gatsby-source-drupal-jsonapi-eci/lib/addNodeTranslations.js
@@ -3,26 +3,20 @@ const addNodeTranslations = nodes =>
     const translations = [];
 
     nodes.forEach(rel => {
-      const isSameResource = rel.id === node.id;
-      const isSameResourceDrupal = rel.drupal_id === node.drupal_id;
-      const isTranslation = rel.default_langcode === false;
+      // Both are nodes with paths.
+      if (
+        rel.path &&
+        rel.path.langcode &&
+        node.path &&
+        node.path.langcode &&
+        rel.path.langcode !== node.path.langcode
+      ) {
+        const isSameResource = rel.id === node.id;
+        const isSameResourceDrupal = rel.drupal_id === node.drupal_id;
 
-      if (!isSameResource && isSameResourceDrupal) {
-        // Regular Drupal way to flag something as a translation.
-        if (isTranslation) {
-          translations.push(rel);
-        }
-        // Untranslated nodes are inheriting path alias information.
-        // Even when they are marked as default_langcode and all the rest, they are not actual translations.
-        if (
-          rel.content_translation_source === 'und' &&
-          rel.default_langcode &&
-          rel.path &&
-          node.path &&
-          node.path.langcode &&
-          rel.path.langcode !== node.path.langcode
-        ) {
-          translations.push(rel);
+        if (!isSameResource && isSameResourceDrupal) {
+          // Gatsby creates content mirroring Drupal paths directly.
+          translations.push(rel.path);
         }
       }
     });
