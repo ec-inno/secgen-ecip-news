71d48565de24c2bb8f3b01652ed999ed34169151
Rob Harings <haringsrob@gmail.com>
2019-06-28 14:23:37 +0200

    Accordion + faq implementation

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,34 +4,49 @@ const languages = require('./src/languages');
 exports.sourceNodes = ({ actions }) => {
   const { createTypes } = actions;
   const typeDefs = `
-  type node__oe_news implements Node {
-    title: String
-    path: Path!
-    oe_teaser: ProcessedField!
-    oe_summary: ProcessedField!
-    field_source: LinkField!
-    langcode: String!
-    oe_publication_date: Date
-    body: ProcessedField!
-    translations: [Path!]!
-  }
-  type Path {
-    alias: String!
-    langcode: String!
-  }
-  type ProcessedField {
-    processed: String!
-  }
-  type LinkField {
-    title: String
-    uri: String
-  }
-  type NodeTranslation implements Node {
-    title: String
-    path: Path!
-    langcode: String
-    translations: [Path!]!
-  }
+    type node__faq implements Node {
+      title: String
+      langcode: String
+      translations: [Path!]!
+      relationships: FaqEntryRelationship!
+    }
+    type FaqEntryRelationship {
+      field_faq_entries: [FaqEntry]
+    }
+    type FaqEntry implements Node {
+      title: String
+      field_answer: ProcessedField
+      langcode: String!
+      translations: [Path!]!
+    }
+    type node__oe_news implements Node {
+      title: String
+      path: Path!
+      oe_teaser: ProcessedField!
+      oe_summary: ProcessedField!
+      field_source: LinkField!
+      langcode: String!
+      oe_publication_date: Date
+      body: ProcessedField!
+      translations: [Path!]!
+    }
+    type Path {
+      alias: String!
+      langcode: String!
+    }
+    type ProcessedField {
+      processed: String!
+    }
+    type LinkField {
+      title: String
+      uri: String
+    }
+    type NodeTranslation implements Node {
+      title: String
+      path: Path!
+      langcode: String
+      translations: [Path!]!
+    }
   `;
   createTypes(typeDefs);
 };
@@ -39,7 +54,10 @@ exports.sourceNodes = ({ actions }) => {
 exports.onCreateNode = ({ node, actions }) => {
   const { createNodeField } = actions;
 
-  if (node.internal.type === 'node__oe_news') {
+  if (
+    node.internal.type === 'node__oe_news' ||
+    node.internal.type === 'node__faq_section'
+  ) {
     // Create a new field on Gatsby side to later store information about translations of a given node.
 
     createNodeField({
@@ -122,6 +140,21 @@ exports.createPages = async ({ graphql, actions }) => {
           languageRegex,
         },
       });
+    });
+  });
+
+  // Create faq sections with paginations for each language.
+  languages.langs.forEach(language => {
+    const languageRegex = `//${language}//`;
+
+    /* eslint-disable-next-line compat/compat */
+    createPage({
+      path: `/${language}/faq`,
+      component: path.resolve('./src/templates/faq-page.jsx'),
+      context: {
+        locale: language,
+        languageRegex,
+      },
     });
   });
 };
