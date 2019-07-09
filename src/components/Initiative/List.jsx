4f1b17754ec081f7791e422ad58a818a3163936c
Kalin Chernev <kalin.chernev@gmail.com>
2019-07-09 17:34:15 +0200

    Calculate, cause service aint provide it

--- src/components/Initiative/List.jsx
+++ src/components/Initiative/List.jsx
@@ -22,71 +22,10 @@ const List = ({ location }) => {
   const [initiatives, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
-  page.push(
-    <div className="ecl-u-mv-xl">
-      <ul className="eci-menu__list">
-        <li
-          className={
-            filter === 'ALL'
-              ? 'eci-menu__option eci-menu__option--is-selected'
-              : 'eci-menu__option'
-          }
-        >
-          <a
-            onClick={e => {
-              e.preventDefault();
-              setFilter('ALL');
-            }}
-            href="#"
-            className="eci-menu__link ecl-link"
-          >
-            All initiatives
-          </a>
-        </li>
-        <li
-          className={
-            filter === 'OPEN'
-              ? 'eci-menu__option eci-menu__option--is-selected'
-              : 'eci-menu__option'
-          }
-        >
-          <a
-            onClick={e => {
-              e.preventDefault();
-              setFilter('OPEN');
-            }}
-            href="#"
-            className="eci-menu__link ecl-link"
-          >
-            Ongoing
-          </a>
-        </li>
-        <li
-          className={
-            filter === 'SUCCESSFUL'
-              ? 'eci-menu__option eci-menu__option--is-selected'
-              : 'eci-menu__option'
-          }
-        >
-          <a
-            onClick={e => {
-              e.preventDefault();
-              setFilter('SUCCESSFUL');
-            }}
-            href="#"
-            className="eci-menu__link ecl-link"
-          >
-            Answered
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
-
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
-      const results = await axios.get(`${endpoint}/get/${filter}`);
+      const results = await axios.get(`${endpoint}/get/all`);
 
       const initiatives = await Promise.all(
         results.data.initiative.map(async basic => {
@@ -116,13 +55,92 @@ const List = ({ location }) => {
     };
 
     fetchData();
-  }, [filter]);
+  }, []);
 
-  const groups = Math.ceil(initiatives.length / itemsPerRow);
+  // The statistics below are calculated on render because service does not provide `_count`.
+  const allCount = initiatives.length;
+  const ongoingCount = initiatives.filter(
+    initiative => initiative.searchEntry['@status'] === 'OPEN'
+  ).length;
+  const answeredCount = initiatives.filter(
+    initiative => initiative.searchEntry['@status'] === 'SUCCESSFUL'
+  ).length;
+
+  // The filtering below should ideally be possible from the service.
+  // If it is, someday, pass `filter` to the `useEffect` to use service calls on change of filter.
+  const initiativesResults =
+    filter === 'ALL'
+      ? initiatives
+      : initiatives.filter(
+          initiative => initiative.searchEntry['@status'] === filter
+        );
+
+  page.push(
+    <div className="ecl-u-mv-xl">
+      <ul className="eci-menu__list">
+        <li
+          className={
+            filter === 'ALL'
+              ? 'eci-menu__option eci-menu__option--is-selected'
+              : 'eci-menu__option'
+          }
+        >
+          <a
+            onClick={e => {
+              e.preventDefault();
+              setFilter('ALL');
+            }}
+            href="#"
+            className="eci-menu__link ecl-link"
+          >
+            All initiatives {allCount ? `(${allCount})` : ''}
+          </a>
+        </li>
+        <li
+          className={
+            filter === 'OPEN'
+              ? 'eci-menu__option eci-menu__option--is-selected'
+              : 'eci-menu__option'
+          }
+        >
+          <a
+            onClick={e => {
+              e.preventDefault();
+              setFilter('OPEN');
+            }}
+            href="#"
+            className="eci-menu__link ecl-link"
+          >
+            Ongoing {ongoingCount ? `(${ongoingCount})` : ''}
+          </a>
+        </li>
+        <li
+          className={
+            filter === 'SUCCESSFUL'
+              ? 'eci-menu__option eci-menu__option--is-selected'
+              : 'eci-menu__option'
+          }
+        >
+          <a
+            onClick={e => {
+              e.preventDefault();
+              setFilter('SUCCESSFUL');
+            }}
+            href="#"
+            className="eci-menu__link ecl-link"
+          >
+            Answered {answeredCount ? `(${answeredCount})` : ''}
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+
+  const groups = Math.ceil(initiativesResults.length / itemsPerRow);
 
   if (isLoading) return <Placeholder location={location} />;
 
-  chunk(initiatives, itemsPerRow).map((group, k) => {
+  chunk(initiativesResults, itemsPerRow).map((group, k) => {
     const groupLength = group.length;
     // If it's either the first or last item, do not add 'md'.
     const rowSpacing =
