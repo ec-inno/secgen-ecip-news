6e65807ab7b1431b0aca19691bedd8ef6847f5ac
Kalin Chernev <kalin.chernev@gmail.com>
2019-07-19 17:20:08 +0200

    Update function

--- src/api/initiatives.js
+++ src/api/initiatives.js
@@ -1,8 +1,18 @@
-const handler = async () => {
-  return {
-    statusCode: 200,
-    body: 'Initiatives',
-  };
-};
+import getInitiatives from '../utils/getInitiatives';
 
-export default handler;
+export const handler = async () => {
+  try {
+    const initiatives = await getInitiatives();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ initiatives }),
+    };
+  } catch (err) {
+    console.log(err); // output to netlify function log
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message }), // Could be a custom message or object i.e. JSON.stringify(err)
+    };
+  }
+};
