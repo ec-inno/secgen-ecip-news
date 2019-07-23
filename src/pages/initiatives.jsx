e57802b84fbb6a019dc6b2af022a31adb4b20cf7
Kalin Chernev <kalin.chernev@gmail.com>
2019-07-23 15:22:00 +0200

    Add checks

--- src/pages/initiatives.jsx
+++ src/pages/initiatives.jsx
@@ -6,10 +6,13 @@ const Initiative = ({ location }) => {
   if (location && location.pathname) {
     const { pathname } = location;
     const pathParts = pathname.split('/').filter(a => a);
-    const initiativeParts = pathParts[1].split('-');
-    const [status, year, number] = initiativeParts;
 
-    console.log(status, year, number);
+    if (pathParts.length && pathParts.length > 1) {
+      const initiativeParts = pathParts[1].split('-');
+      const [status, year, number] = initiativeParts;
+
+      console.log(status, year, number);
+    }
   }
 
   return (
