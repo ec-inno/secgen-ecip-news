9a726df4f08a1f5c2736f7278bef860afa23ff97
Kalin Chernev <kalin.chernev@gmail.com>
2019-07-30 12:51:08 +0200

    Language fallback for pages

--- gatsbyNodeApi/createPages.js
+++ gatsbyNodeApi/createPages.js
@@ -1,17 +1,19 @@
 const path = require('path');
-const { languages } = require('../languages');
+const { languages, defaultLangKey } = require('../languages');
 
 const createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
 
   const itemsPerPage = 10;
   const newsPerLanguage = {};
+  const basicPagesPerLanguage = {};
 
   const result = await graphql(`
     query getDrupalContent {
       allNodeOePage {
         edges {
           node {
+            id
             path {
               alias
               langcode
@@ -33,22 +35,91 @@ const createPages = async ({ graphql, actions }) => {
   const oeNews = allNodeOeNews.edges;
   const oePages = allNodeOePage.edges;
 
-  oePages.forEach(async ({ node }) => {
-    const { path: pathauto } = node;
-    const { alias, langcode } = pathauto;
+  // Prepare information about page nodes and their translations.
+  oePages.forEach(({ node }) => {
+    const langcode = node.id.split('/')[1];
 
+    if (!basicPagesPerLanguage[langcode]) {
+      basicPagesPerLanguage[langcode] = [];
+    }
+
+    const nodeExists = basicPagesPerLanguage[langcode].find(
+      n => n.id === node.id && n.path.langcode === langcode
+    );
+
+    // Source plugin will create nodes which are not Drupal content and we don't need these.
+    // For this reason, double-check the language from API is node's language, if it's not, it's a source plugin content.
+    const isActualNode = node.path.langcode === langcode;
+
+    if (!nodeExists && isActualNode) basicPagesPerLanguage[langcode].push(node);
+  });
+
+  // For each item of the default language (English)
+  const basicPages = basicPagesPerLanguage[defaultLangKey];
+
+  // Create page for the default language and available translations.
+  // When a translation is not available, fallback to English.
+  basicPages.forEach(node => {
+    const { path: pathauto, id: nodeId } = node;
+    const { alias: aliasDefault, langcode: langcodeDefault } = pathauto;
+    const uid = nodeId.split('/')[2];
+
+    // Create page for default language.
     createPage({
-      // Drupal's pathauto requires that aliases are starting with `/`
-      path: `/${langcode}${alias}`,
+      path: `/${langcodeDefault}${aliasDefault}`,
       component: path.resolve('./src/templates/basic-page.jsx'),
       context: {
-        locale: langcode,
-        alias,
+        locale: langcodeDefault,
+        alias: aliasDefault,
       },
+    });
+
+    // Create pages for the other languages.
+    const otherLanguages = languages.map(l => l.lang);
+    // Remove default language from the list.
+    otherLanguages.splice(otherLanguages.indexOf(defaultLangKey), 1);
+
+    otherLanguages.forEach(language => {
+      let alias = aliasDefault;
+      let locale = langcodeDefault;
+      let pagePath = `/${language}${aliasDefault}`;
+
+      const translation = basicPagesPerLanguage[language].find(n =>
+        n.id.includes(uid)
+      );
+
+      if (translation) {
+        const { path: pathTranslation } = translation;
+        const {
+          alias: aliasTranslation,
+          langcode: localeTranslation,
+        } = pathTranslation;
+
+        alias = aliasTranslation;
+        locale = localeTranslation;
+        pagePath = `/${localeTranslation}${aliasTranslation}`;
+      }
+      // Create a "placeholder" in the given `language` path and default alias.
+      // Passing `langcodeDefault` to fallback to default language for content.
+      else {
+        alias = aliasDefault;
+        locale = langcodeDefault;
+        pagePath = `/${language}${aliasDefault}`;
+      }
+
+      // Create the page for the non-default language.
+      createPage({
+        path: pagePath,
+        component: path.resolve('./src/templates/basic-page.jsx'),
+        context: {
+          locale,
+          alias,
+        },
+      });
     });
   });
 
-  // Prepare pagination.
+  // Prepare pagination for news content type.
   oeNews.forEach(({ node }) => {
     const langcode = node.id.split('/')[1];
 
@@ -63,6 +134,7 @@ const createPages = async ({ graphql, actions }) => {
     if (!nodeExists) newsPerLanguage[langcode].push(node);
   });
 
+  // Create per-language pages containing collections of nodes.
   languages
     .map(l => l.lang)
     .forEach(language => {
