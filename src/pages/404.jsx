789733b019d24d68874253b9d7556aca2724e081
Kalin Chernev <kalin.chernev@gmail.com>
2019-08-07 10:03:08 +0200

    Add 404 pages

---
+++ src/pages/404.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import SEO from '../components/SEO';
+import getCurrentLanguage from '../utils/getCurrentLanguage';
+
+import ForumBanner from '../components/ForumBanner';
+
+const NotFound = ({ location }) => {
+  const currentLanguage = getCurrentLanguage(location);
+
+  return (
+    <>
+      <SEO title="Not found" />
+
+      <main>
+        <div className="ecl-container">
+          <div className="ecl-row ecl-u-mt-l">
+            <div className="ecl-col-sm-12 ecl-col-md-12">
+              <h1 className="ecl-u-type-heading-1">404</h1>
+              <p className="ecl-u-type-paragraph">
+                Not found. Go <Link to={`/${currentLanguage}`}>home</Link>.
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
+      <ForumBanner location={location} />
+    </>
+  );
+};
+
+export default NotFound;
