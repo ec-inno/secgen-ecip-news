644199a2b97fde3cf80d3e2ec425f9ce3c397e07
Kalin Chernev <kalin.chernev@gmail.com>
2019-08-09 13:11:10 +0200

    Remove deps

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,12 +4,3 @@ import { ContextProviderWithReducer } from './src/Context';
 export const wrapRootElement = ({ element }) => {
   return <ContextProviderWithReducer>{element}</ContextProviderWithReducer>;
 };
-
-export const onClientEntry = () => {
-  // @see https://www.gatsbyjs.org/packages/gatsby-background-image/#important
-  // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
-  if (typeof window.IntersectionObserver === `undefined`) {
-    import(`intersection-observer`);
-    console.log(`# IntersectionObserver is polyfilled!`);
-  }
-};
