f4c0dde479c9b720e58879ab862e87706abd3b9f
Kalin Chernev <kalin.chernev@gmail.com>
2019-08-26 16:36:51 +0200

    Start using Counters API

--- src/components/Initiative/Counter.jsx
+++ src/components/Initiative/Counter.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import getCurrentLanguage from '../../utils/getCurrentLanguage';
 import getDefaultLanguage from '../../utils/getDefaultLanguage';
@@ -9,19 +10,37 @@ const Counter = ({ location }) => {
   const language = getCurrentLanguage(location) || getDefaultLanguage();
   const translation = require(`../../../translations/counter/${language}.json`);
 
+  const { GATSBY_SITE_BASE_URL: api } = process.env;
+
+  const [stats, setStats] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`${api}/${language}/api/counters`)
+      .then(data => {
+        setStats(data);
+      })
+      .catch(e => {
+        console.error(`Failed to fetch initiatives statistics`, e);
+      });
+  }, [language]);
+
   return (
     <p className="ecl-u-type-paragraph ecl-u-mt-l ecl-u-d-flex ecl-u-justify-content-between">
       <span className="ecl-u-d-flex ecl-u-align-items-center">
         <Icon className="ecl-u-mr-xs" shape="general--file" />
-        ... {translation.registration_requests}
+        {stats.requests ? stats.requests : '...'}{' '}
+        {translation.registration_requests}
       </span>
       <span className="ecl-u-d-flex ecl-u-align-items-center">
         <Icon className="ecl-u-mr-xs" shape="general--edit" />
-        ... {translation.initiatives_registered}
+        {stats.registered ? stats.registered : '...'}{' '}
+        {translation.initiatives_registered}
       </span>
       <span className="ecl-u-d-flex ecl-u-align-items-center">
         <Icon className="ecl-u-mr-xs" shape="ui--check-filled" />
-        ... {translation.successful_initiatives}
+        {stats.successful ? stats.successful : '...'}{' '}
+        {translation.successful_initiatives}
       </span>
     </p>
   );
