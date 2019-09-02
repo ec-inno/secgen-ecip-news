2662373510bb0665c0e4fc54ef279038ba611b95
Kalin Chernev <kalin.chernev@gmail.com>
2019-09-02 16:51:05 +0200

    Display existing pages

--- src/pages/sitemap.jsx
+++ src/pages/sitemap.jsx
@@ -1,13 +1,49 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import { Link as LinkInternal } from 'gatsby';
+import LinkExternal from '../components/Link/LinkEcl';
 
 import getCurrentLanguage from '../utils/getCurrentLanguage';
 
 import SEO from '../components/SEO';
 
-// Update title and body to use dynamic content coming from Drupal's menu.
-const Sitemap = ({ location }) => {
+const Sitemap = ({ location, data }) => {
+  const list = [];
+  const remove = '/multisite/ecip/';
   const currentLanguage = getCurrentLanguage(location);
+
+  const menuTmp = require('../components/Menu/data.json'); // From query when API comes
+
+  const menu = menuTmp.length
+    ? menuTmp
+        .filter(link => link.attributes.enabled)
+        .map(link => ({
+          title: link.attributes.title,
+          hrefFormatted: link.attributes.fetched_alias
+            .replace(remove, '')
+            .replace(`${currentLanguage}/`, `/${currentLanguage}/`),
+          external: link.attributes.external,
+        }))
+        .map(link => ({
+          ...link,
+          href:
+            link.hrefFormatted === currentLanguage
+              ? `/${currentLanguage}`
+              : link.hrefFormatted,
+        }))
+    : [];
+
+  const pages =
+    data.allNodeOePage && data.allNodeOePage.edges
+      ? data.allNodeOePage.edges.map(({ node }) => ({
+          title: node.title,
+          href: `/${currentLanguage}${node.path.alias}`,
+        }))
+      : [];
+
+  list.push(...menu);
+  list.push(...pages);
+
+  console.log('list', list);
 
   return (
     <>
@@ -18,106 +54,33 @@ const Sitemap = ({ location }) => {
           <div className="ecl-row ecl-u-mt-l">
             <div className="ecl-col-sm-12 ecl-col-md-12">
               <h1 className="ecl-u-type-heading-1">Sitemap</h1>
-              <ul class="ecl-unordered-list">
-                <li class="ecl-unordered-list__item">
-                  <Link
-                    to={`/${currentLanguage}/`}
-                    className="ecl-link ecl-link--standalone"
-                  >
-                    Home
-                  </Link>
-                </li>
-                <li class="ecl-unordered-list__item">
-                  <Link
-                    to={`/${currentLanguage}/news`}
-                    className="ecl-link ecl-link--standalone"
-                  >
-                    News
-                  </Link>
-                </li>
-                <li class="ecl-unordered-list__item">
-                  <Link
-                    to={`/${currentLanguage}/how-to-start`}
-                    className="ecl-link ecl-link--standalone"
-                  >
-                    How to start
-                  </Link>
-                </li>
-                <li class="ecl-unordered-list__item">
-                  <Link
-                    to={`/${currentLanguage}/faq`}
-                    className="ecl-link ecl-link--standalone"
-                  >
-                    FAQ
-                  </Link>
-                </li>
-                <li class="ecl-unordered-list__item">
-                  <Link
-                    to={`/${currentLanguage}/contact`}
-                    className="ecl-link ecl-link--standalone"
-                  >
-                    Contact
-                  </Link>
-                </li>
-                <li class="ecl-unordered-list__item">
-                  <Link
-                    to={`/${currentLanguage}/sitemap`}
-                    className="ecl-link ecl-link--standalone"
-                  >
-                    Sitemap
-                  </Link>
-                </li>
-                <li class="ecl-unordered-list__item">
-                  <Link
-                    to={`/${currentLanguage}/communication-material`}
-                    className="ecl-link ecl-link--standalone"
-                  >
-                    Communication material
-                  </Link>
-                </li>
-                <li class="ecl-unordered-list__item">
-                  <Link
-                    to={`/${currentLanguage}/how-it-works`}
-                    className="ecl-link ecl-link--standalone"
-                  >
-                    How it works
-                  </Link>
-                  <ul class="ecl-unordered-list">
-                    <li class="ecl-unordered-list__item">
-                      <Link
-                        to={`/${currentLanguage}/how-it-works/regulatory-framework`}
-                        className="ecl-link ecl-link--standalone"
-                      >
-                        Regulatory framework
-                      </Link>
-                    </li>
-                    <li class="ecl-unordered-list__item">
-                      <Link
-                        to={`/${currentLanguage}/how-it-works/implementation-national-level`}
-                        className="ecl-link ecl-link--standalone"
-                      >
-                        Implementation national level
-                      </Link>
-                    </li>
-                    <li class="ecl-unordered-list__item">
-                      <Link
-                        to={`/${currentLanguage}/how-it-works/data-protection`}
-                        className="ecl-link ecl-link--standalone"
-                      >
-                        Data protection
-                      </Link>
-                    </li>
-                    <li class="ecl-unordered-list__item">
-                      <Link
-                        to={`/${currentLanguage}/how-it-works/history`}
-                        className="ecl-link ecl-link--standalone"
-                      >
-                        History
-                      </Link>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
+              {list.length ? (
+                <ul className="ecl-unordered-list">
+                  {list.map((listItem, key) => {
+                    return (
+                      <li key={key} className="ecl-unordered-list__item">
+                        {listItem.external ? (
+                          <LinkExternal
+                            href={listItem.href}
+                            className="ecl-link ecl-link--standalone"
+                          >
+                            {listItem.title}
+                          </LinkExternal>
+                        ) : (
+                          <LinkInternal
+                            to={listItem.href}
+                            className="ecl-link ecl-link--standalone"
+                          >
+                            {listItem.title}
+                          </LinkInternal>
+                        )}
+                      </li>
+                    );
+                  })}
+                </ul>
+              ) : (
+                ''
+              )}
             </div>
           </div>
         </div>
@@ -125,5 +88,27 @@ const Sitemap = ({ location }) => {
     </>
   );
 };
+
+export const query = graphql`
+  query getPages($locale: String!, $languageRegex: String!) {
+    allNodeOePage(
+      filter: {
+        id: { regex: $languageRegex }
+        path: { langcode: { eq: $locale } }
+      }
+      sort: { fields: path___alias, order: ASC }
+    ) {
+      edges {
+        node {
+          id
+          title
+          path {
+            alias
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default Sitemap;
