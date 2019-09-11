6304f56ae726af37bc318f22b022f320982bf24e
Kalin Chernev <kalin.chernev@gmail.com>
2019-09-11 15:03:57 +0200

    Save href paths in a relative form.

--- packages/eci-cli/src/commands/api/download.js
+++ packages/eci-cli/src/commands/api/download.js
@@ -1,6 +1,15 @@
 module.exports = {
   name: 'download',
   run: async toolbox => {
+    // We care only for these at the moment.
+    const entityTypes = [
+      'node--faq',
+      'node--faq_section',
+      'node--oe_news',
+      'node--oe_page',
+      'menu',
+    ];
+
     const { SITE_BASE_URL: baseURL } = process.env;
 
     if (!baseURL) {
@@ -18,6 +27,27 @@ module.exports = {
     } = toolbox;
 
     const client = http.create({ baseURL });
+
+    const saveDataLocally = (resourcePath, data) => {
+      const updated = JSON.parse(JSON.stringify(data));
+
+      const convertLinksToRelative = o => {
+        Object.keys(o).forEach(k => {
+          // Make the change.
+          if (k === 'href') {
+            o[k] = o[k].replace(baseURL, '');
+          }
+          // Recurse if nested.
+          if (o[k] !== null && typeof o[k] === 'object') {
+            convertLinksToRelative(o[k]);
+          }
+        });
+      };
+
+      convertLinksToRelative(updated);
+
+      write(path(`drupal_jsonapi/${resourcePath}/data.json`), updated);
+    };
 
     // Inline utility to get links data.
     const getLinkData = async (url, data = []) => {
@@ -42,7 +72,8 @@ module.exports = {
       }
 
       data = data.concat(linkData.data.data);
-      write(path(`drupal_jsonapi/${resourcePath}/data.json`), data);
+
+      saveDataLocally(resourcePath, data);
 
       if (linkData.data.links.next) {
         data = await getLinkData(linkData.data.links.next, data);
@@ -51,31 +82,32 @@ module.exports = {
       return data;
     };
 
-    info('Making a local copy of a JSONAPI endpoint ...');
+    info('Making a local copy of the JSONAPI ...');
 
     try {
       languages.map(async language => {
         const resource = `${language}/api`;
         const response = await client.get(`/${resource}`);
 
-        write(path(`drupal_jsonapi/${resource}/data.json`), response.data);
+        saveDataLocally(resource, response.data);
 
         const { links } = response.data;
 
         if (links) {
-          // Correction for Drupal's JSON API issue dropping language.
+          // Correction for Drupal's JSON API issue dropping the language.
+          // Make the correction at this stage in order to follow `links` crrectly.
           const defaultEndpoint = `${baseURL}/api`;
           const languageEndpoint = `${baseURL}/${language}/api`;
 
           Object.keys(links).map(async type => {
-            if (type !== 'self') {
-              const url = links[type].href.replace(
-                defaultEndpoint,
-                languageEndpoint
-              );
+            if (type === 'self' || !entityTypes.includes(type)) return;
 
-              getLinkData(url);
-            }
+            const url = links[type].href.replace(
+              defaultEndpoint,
+              languageEndpoint
+            );
+
+            getLinkData(url);
           });
         }
       });
