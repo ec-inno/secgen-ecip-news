f094121bd6b4019141040246bad7d4c2d94dc952
Kalin Chernev <kalin.chernev@gmail.com>
2019-09-23 10:31:21 +0200

    Human-readable and translatable list of countries

---
+++ src/components/Initiative/utils/getCountries.js
@@ -0,0 +1,29 @@
+const getCountriesTranslated = t => {
+  return [
+    { value: 'bg', label: t('Bulgaria') },
+    { value: 'es', label: t('Spain') },
+    { value: 'cs', label: t('Czech Republic') },
+    { value: 'da', label: t('Denmark') },
+    { value: 'de', label: t('Germany') },
+    { value: 'et', label: t('Estonia') },
+    { value: 'el', label: t('Greece') },
+    { value: 'fr', label: t('France') },
+    { value: 'ga', label: t('Ireland') },
+    { value: 'hr', label: t('Croatia') },
+    { value: 'it', label: t('Italia') },
+    { value: 'lv', label: t('Latvia') },
+    { value: 'lt', label: t('Lithuania') },
+    { value: 'hu', label: t('Hungary') },
+    { value: 'mt', label: t('Malta') },
+    { value: 'nl', label: t('Netherlands') },
+    { value: 'pl', label: t('Poland') },
+    { value: 'pt', label: t('Portugal') },
+    { value: 'ro', label: t('Romania') },
+    { value: 'sk', label: t('Slovakia') },
+    { value: 'sl', label: t('Slovenia') },
+    { value: 'fi', label: t('Finland') },
+    { value: 'sv', label: t('Sweden') },
+  ];
+};
+
+export default getCountriesTranslated;
